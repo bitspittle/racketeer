f85e9be75c290e29675944ee7e3b2543a164ec17
David Herman <bitspittle@gmail.com>
2022-03-26 17:17:50 -0700

    Change shop to cost influence

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/ExpandShopCommand.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/ExpandShopCommand.kt
@@ -7,8 +7,7 @@ class ExpandShopCommand(ctx: GameContext) : Command {
     init {
         require(ctx.state.shopTier < 4) { "Shop already at max tier - Expand Shop item should be hidden" }
     }
-    override val title = "Expand shop ${ctx.describers.describeCash(ctx.config.shopPrices[ctx.state.shopTier + 1])}"
+    override val title = "Expand shop ${ctx.describers.describeInfluence(ctx.config.shopPrices[ctx.state.shopTier + 1])}"
 
     override val description = "Expand the shop, adding an additional card for sale and increasing the quality of cards that it sells"
 }
-
