d2f2baa489f4a48a9c04ed868f964151b17e57de
David Herman <bitspittle@gmail.com>
2022-03-29 21:51:12 -0700

    Fix bug with discard command going to deck view

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/BrowseDiscardCommand.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/BrowseDiscardCommand.kt
@@ -3,6 +3,7 @@ package dev.bitspittle.racketeer.console.command.commands
 import dev.bitspittle.racketeer.console.game.GameContext
 import dev.bitspittle.racketeer.console.command.Command
 import dev.bitspittle.racketeer.console.view.views.BrowseDeckView
+import dev.bitspittle.racketeer.console.view.views.BrowseDiscardView
 
 class BrowseDiscardCommand(ctx: GameContext) : Command(ctx) {
     override val title = "Browse discard (${ctx.state.discard.cards.size})"
@@ -11,7 +12,7 @@ class BrowseDiscardCommand(ctx: GameContext) : Command(ctx) {
 
     override fun invoke(): Boolean {
         return if (ctx.state.discard.cards.isNotEmpty()) {
-            ctx.viewStack.pushView(BrowseDeckView(ctx))
+            ctx.viewStack.pushView(BrowseDiscardView(ctx))
             true
         } else {
             false
