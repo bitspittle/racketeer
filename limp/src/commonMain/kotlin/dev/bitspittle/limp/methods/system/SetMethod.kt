7d2deee00a94dbfe5aa12e19b61862dfc8228883
David Herman <bitspittle@gmail.com>
2022-04-02 15:17:20 -0700

    Nit: Add header comment

--- limp/src/commonMain/kotlin/dev/bitspittle/limp/methods/system/SetMethod.kt
+++ limp/src/commonMain/kotlin/dev/bitspittle/limp/methods/system/SetMethod.kt
@@ -6,6 +6,9 @@ import dev.bitspittle.limp.Method
 import dev.bitspittle.limp.Value
 import dev.bitspittle.limp.types.Expr
 
+/**
+ * Store a value with some variable name label.
+ */
 class SetMethod : Method("set", 2) {
     override fun invoke(env: Environment, params: List<Value>, options: Map<String, Value>, rest: List<Value>): Value {
         val nameExpr = env.expectConvert<Expr>(params[0])
