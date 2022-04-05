ef7ae789660975bd6f25df473024edbcb5279375
David Herman <bitspittle@gmail.com>
2022-04-05 15:22:48 -0700

    Add MutablePile converter
    This will allow us to shuffle piles in place

--- scripting/src/commonMain/kotlin/dev/bitspittle/racketeer/scripting/EnvironmentExtensions.kt
+++ scripting/src/commonMain/kotlin/dev/bitspittle/racketeer/scripting/EnvironmentExtensions.kt
@@ -1,8 +1,10 @@
 package dev.bitspittle.racketeer.scripting
 
 import dev.bitspittle.limp.Environment
+import dev.bitspittle.racketeer.model.card.Card
 import dev.bitspittle.racketeer.model.game.GameState
 import dev.bitspittle.racketeer.scripting.converters.PileToCardsConverter
+import dev.bitspittle.racketeer.scripting.converters.MutablePileToCardsConverter
 import dev.bitspittle.racketeer.scripting.methods.DbgMethod
 import dev.bitspittle.racketeer.scripting.methods.GameAddMethod
 import dev.bitspittle.racketeer.scripting.types.GameService
@@ -11,14 +13,16 @@ import dev.bitspittle.racketeer.scripting.types.GameService
  * Add a bunch of game-specific methods and other values here.
  */
 fun Environment.installGameLogic(service: GameService) {
+    addConverter(MutablePileToCardsConverter())
     addConverter(PileToCardsConverter())
 
     addMethod(DbgMethod(service))
     addMethod(GameAddMethod(service))
 }
 
-fun Environment.withCardVariables(gameState: GameState, block: () -> Unit) {
+fun Environment.withCardVariables(gameState: GameState, card: Card, block: () -> Unit) {
     scoped {
+        storeValue("\$this", card)
         storeValue("\$deck", gameState.deck)
         storeValue("\$hand", gameState.hand)
         storeValue("\$street", gameState.street)
---
+++ scripting/src/commonMain/kotlin/dev/bitspittle/racketeer/scripting/converters/MutablePileToCardsConverter.kt
@@ -0,0 +1,15 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package dev.bitspittle.racketeer.scripting.converters
+
+import dev.bitspittle.limp.Converter
+import dev.bitspittle.racketeer.model.card.Card
+import dev.bitspittle.racketeer.model.card.MutablePile
+import kotlin.reflect.KClass
+
+// Note: Must define before PileToCardsConverter, or else that one will always win
+class MutablePileToCardsConverter : Converter<MutableList<Card>>(MutableList::class as KClass<MutableList<Card>>) {
+    override fun convert(value: Any): MutableList<Card>? {
+        return (value as? MutablePile)?.cards
+    }
+}
