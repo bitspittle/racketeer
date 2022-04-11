395548232f78e58c21d03bce287d1f0f30884fd7
David Herman <bitspittle@gmail.com>
2022-04-10 22:54:14 -0700

    Browse the street
    ... and its effects

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/PlayCardsView.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/PlayCardsView.kt
@@ -2,14 +2,15 @@ package dev.bitspittle.racketeer.console.view.views
 
 import com.varabyte.kotter.foundation.text.textLine
 import com.varabyte.kotter.runtime.render.RenderScope
-import dev.bitspittle.racketeer.console.game.GameContext
 import dev.bitspittle.racketeer.console.command.Command
 import dev.bitspittle.racketeer.console.command.commands.*
+import dev.bitspittle.racketeer.console.game.GameContext
 import dev.bitspittle.racketeer.console.view.View
 
 class PlayCardsView(ctx: GameContext) : View(ctx) {
     override fun createCommands(): List<Command> =
         List(ctx.state.hand.cards.size) { i -> PlayCardCommand(ctx, i) } + listOf(
+            BrowseStreetCommand(ctx),
             BrowseShopCommand(ctx),
             BrowseDeckCommand(ctx),
             BrowseDiscardCommand(ctx),
@@ -22,6 +23,9 @@ class PlayCardsView(ctx: GameContext) : View(ctx) {
             textLine("The Street:")
             ctx.state.street.cards.forEach { card ->
                 textLine("- ${ctx.describer.describe(card, concise = true)}")
+            }
+            if (ctx.state.streetEffects.isNotEmpty()) {
+                textLine("... and ${ctx.state.streetEffects.size} effect(s)")
             }
             textLine()
         }
