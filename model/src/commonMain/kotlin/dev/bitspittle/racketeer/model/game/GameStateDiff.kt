a80e7494125d2a2f241e1e7b11fd1c500eb9e1e8
David Herman <bitspittle@gmail.com>
2022-04-16 17:05:52 -0700

    Report discard reshuffling in aggregate
    This avoids late game notification dumps that are HUGE when you
    go to draw a new hand, because your discard pile has gotten so
    big by then.

--- model/src/commonMain/kotlin/dev/bitspittle/racketeer/model/game/GameStateDiff.kt
+++ model/src/commonMain/kotlin/dev/bitspittle/racketeer/model/game/GameStateDiff.kt
@@ -76,17 +76,48 @@ class GameStateDiff(
         allCardsBefore: Map<Uuid, Card>,
         allCardsAfter: Map<Uuid, Card>
     ) {
-        allCardsAfter
+        // List of all cards that moved from some pile -> pile
+        val pileTransfers = allCardsAfter
             .asSequence()
             .filter { (id, _) -> allCardsBefore.contains(id) }
-            .filter { (id, card) ->
-                gameStateBefore.pileFor(allCardsBefore.getValue(id))!!.id != gameStateAfter.pileFor(card)!!.id
-            }.map { (_, card) -> card }
-            .forEach { card ->
-                val cardTitle = describer.describe(card, concise = true)
-                val pileFromDesc = describer.describe(gameStateBefore, gameStateBefore.pileFor(card)!!, title = false)
-                val pileToDesc = describer.describe(gameStateAfter, gameStateAfter.pileFor(card)!!, title = false)
-                reportLine("$cardTitle moved from $pileFromDesc to $pileToDesc.")
+            .map { (_, card) -> card }
+            .groupBy { card ->
+                gameStateBefore.pileFor(card)!! to gameStateAfter.pileFor(card)!!
+            }
+            .filter { (transfer, cards) ->
+                transfer.first.id != transfer.second.id
+            }
+
+        val reshuffleTransfer = gameStateBefore.discard to gameStateAfter.deck
+        var reshuffleHappened = false
+        // Report discard reshuffling in aggregate, since it's not very interesting to see 20 "X moved from discard
+        // to your deck" notifications when you draw cards in the late game
+        pileTransfers[reshuffleTransfer]?.let {
+            reportLine("Your discard pile (${gameStateBefore.discard.cards.size}) was reshuffled into your deck to refill it.")
+            reshuffleHappened = true
+        }
+
+        pileTransfers
+            .filter { (transfer, _) -> transfer != reshuffleTransfer }
+            .forEach { (transfer, cards) ->
+                val (pileFrom, pileTo) = transfer.let { transfer ->
+                    // In the early game, your deck is so small that when you draw cards on the second round, you will
+                    // trigger a discard reshuffle that goes into your deck and THEN into your hand. It's confusing to
+                    // report this as "A card went from the discard pile into your hand" even though that's technically
+                    // true. But to the user, they think more the card should have gone from the discard pile into their
+                    // deck, and then the deck into their hand.
+                    // We only do this interception if a reshuffle happened. Otherwise, maybe there will be a card whose
+                    // effect is to recover a card from the discard pile.
+                    if (transfer.first == gameStateBefore.discard && reshuffleHappened) {
+                        gameStateBefore.deck to transfer.second
+                    } else transfer
+                }
+                cards.forEach { card ->
+                    val cardTitle = describer.describe(card, concise = true)
+                    val pileFromDesc = describer.describe(gameStateBefore, pileFrom, title = false)
+                    val pileToDesc = describer.describe(gameStateAfter, pileTo, title = false)
+                    reportLine("$cardTitle moved from $pileFromDesc into $pileToDesc.")
+                }
             }
     }
 
