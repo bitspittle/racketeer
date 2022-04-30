bb9f441bedd9895d1f52641a30fd331f3b5400a3
David Herman <bitspittle@gmail.com>
2022-04-30 11:06:50 -0700

    Nit: Try using "?" same as the number of characters in the card name

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/game/ViewCardTemplateCommand.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/game/ViewCardTemplateCommand.kt
@@ -10,11 +10,11 @@ fun CardTemplate.shouldMask(ctx: GameContext) = ctx.settings.maskCards && !ctx.c
  * A no-op command used when read-only viewing a card template
  */
 class ViewCardTemplateCommand(
-    ctx: GameContext, private val card: CardTemplate, extra: String? = null
+    ctx: GameContext, card: CardTemplate, extra: String? = null
 ) : Command(ctx) {
     private val shouldMaskCard = card.shouldMask(ctx)
 
-    override val title = if (shouldMaskCard) "?".repeat(10) else ctx.describer.describeCard(card, concise = true)
+    override val title = if (shouldMaskCard) "?".repeat(card.name.length) else ctx.describer.describeCard(card, concise = true)
     override val extra = if (shouldMaskCard) null else extra
     override val description = if (shouldMaskCard) "You must own this card at least once to see its details." else ctx.describer.describeCard(card)
 }
