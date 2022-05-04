dbb9ec3b3e45333de7003dfc74fd67d57157b6f5
David Herman <bitspittle@gmail.com>
2022-05-04 10:55:58 -0700

    Bake a snapshot of game data into the resources directory
    This can still be overridden by a local gamedata.yaml

--- console/src/main/kotlin/main.kt
+++ console/src/main/kotlin/main.kt
@@ -1,11 +1,16 @@
 import dev.bitspittle.racketeer.console.game.GameSession
 import dev.bitspittle.racketeer.model.game.GameData
 import kotlin.io.path.Path
+import kotlin.io.path.exists
 import kotlin.io.path.readText
 
 fun main() {
-    val gameData = with(Path("gamedata.yaml").readText(Charsets.UTF_8)) {
-        GameData.decodeFromString(this)
+    val gameData = run {
+        // For users, we will just use the bundled gamedata.yaml. However, if you know what you're doing, you can
+        // create a local gamedata.yaml and iterate with that one instead.
+        val gameDataStr = Path("gamedata.yaml").takeIf { it.exists() }?.readText()
+            ?: GameSession::class.java.getResourceAsStream("/gamedata.yaml")!!.readAllBytes().decodeToString()
+        GameData.decodeFromString(gameDataStr)
     }
 
     GameSession(gameData).start()
