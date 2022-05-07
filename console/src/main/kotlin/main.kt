40988c13c079dc61ce7c436b7d8c6b8a71d7f301
David Herman <bitspittle@gmail.com>
2022-05-06 17:57:17 -0700

    Clear the console on startup

--- console/src/main/kotlin/main.kt
+++ console/src/main/kotlin/main.kt
@@ -4,6 +4,11 @@ import kotlin.io.path.Path
 import kotlin.io.path.exists
 import kotlin.io.path.readText
 
+private fun clearConsole() {
+    print("\u001b[H\u001b[2J")
+    System.out.flush()
+}
+
 fun main() {
     val gameData = run {
         // For users, we will just use the bundled gamedata.yaml. However, if you know what you're doing, you can
@@ -13,5 +18,6 @@ fun main() {
         GameData.decodeFromString(gameDataStr)
     }
 
+    clearConsole()
     GameSession(gameData).start()
 }
