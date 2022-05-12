101dfc7bbc9daa4a1eb5f02163b306dfe79ec79d
David Herman <bitspittle@gmail.com>
2022-05-11 21:48:02 -0700

    Move card actions to "Browse all piles" view

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/game/cards/BrowsePilesView.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/game/cards/BrowsePilesView.kt
@@ -1,12 +1,37 @@
 package dev.bitspittle.racketeer.console.view.views.game.cards
 
+import com.varabyte.kotter.foundation.text.textLine
+import com.varabyte.kotter.runtime.MainRenderScope
 import dev.bitspittle.racketeer.console.command.Command
 import dev.bitspittle.racketeer.console.command.commands.game.cards.*
 import dev.bitspittle.racketeer.console.game.GameContext
+import dev.bitspittle.racketeer.console.user.inAdminModeAndShowDebugInfo
 import dev.bitspittle.racketeer.console.view.views.game.GameView
 import dev.bitspittle.racketeer.model.game.isGameOver
 
 class BrowsePilesView(ctx: GameContext) : GameView(ctx) {
+    override fun MainRenderScope.renderContentUpper() {
+        if (ctx.state.effects.items.isNotEmpty()) {
+            textLine("Active effects:")
+            ctx.state.effects.items.forEach { effect ->
+                textLine("- ${effect.desc ?: effect.expr}")
+                if (ctx.settings.inAdminModeAndShowDebugInfo) {
+                    textLine("  Lifetime: ${effect.lifetime.name.lowercase()}, Event: ${effect.event.name.lowercase()}")
+                    if (effect.data != null) {
+                        textLine("  Data: ${effect.data}")
+                    }
+                    if (effect.testExpr != null) {
+                        textLine("  Test: ${effect.testExpr}")
+                    }
+                    if (effect.desc != effect.expr) {
+                        textLine("  ${effect.expr}")
+                    }
+                }
+            }
+            textLine()
+        }
+    }
+
     override fun createCommands() = (if (!ctx.state.isGameOver) {
         listOf(BrowseShopCommand(ctx))
     } else emptyList<Command>()) + listOf(
--- console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/game/cards/BrowseStreetView.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/game/cards/BrowseStreetView.kt
@@ -1,11 +1,8 @@
 package dev.bitspittle.racketeer.console.view.views.game.cards
 
-import com.varabyte.kotter.foundation.text.textLine
-import com.varabyte.kotter.runtime.MainRenderScope
 import dev.bitspittle.racketeer.console.command.Command
 import dev.bitspittle.racketeer.console.command.commands.game.cards.ViewCardCommand
 import dev.bitspittle.racketeer.console.game.GameContext
-import dev.bitspittle.racketeer.console.user.inAdminModeAndShowDebugInfo
 import dev.bitspittle.racketeer.console.view.views.game.GameView
 
 class BrowseStreetView(ctx: GameContext) : GameView(ctx) {
@@ -17,26 +14,4 @@ class BrowseStreetView(ctx: GameContext) : GameView(ctx) {
 
     override fun createCommands(): List<Command> =
         ctx.state.street.cards.map { card -> ViewCardCommand(ctx, card) }
-
-    override fun MainRenderScope.renderContentUpper() {
-        if (ctx.state.effects.items.isNotEmpty()) {
-            textLine("Active effects:")
-            ctx.state.effects.items.forEach { effect ->
-                textLine("- ${effect.desc ?: effect.expr}")
-                if (ctx.settings.inAdminModeAndShowDebugInfo) {
-                    textLine("  Lifetime: ${effect.lifetime.name.lowercase()}, Event: ${effect.event.name.lowercase()}")
-                    if (effect.data != null) {
-                        textLine("  Data: ${effect.data}")
-                    }
-                    if (effect.testExpr != null) {
-                        textLine("  Test: ${effect.testExpr}")
-                    }
-                    if (effect.desc != effect.expr) {
-                        textLine("  ${effect.expr}")
-                    }
-                }
-            }
-            textLine()
-        }
-    }
 }
