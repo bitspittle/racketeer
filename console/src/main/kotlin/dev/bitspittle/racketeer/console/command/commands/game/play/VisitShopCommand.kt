19e6f6199f44870b3cd40be59f9fe16720e1b4f2
David Herman <bitspittle@gmail.com>
2022-05-12 14:02:28 -0700

    Highlight shop when you can afford something

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/game/play/VisitShopCommand.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/game/play/VisitShopCommand.kt
@@ -3,8 +3,18 @@ package dev.bitspittle.racketeer.console.command.commands.game.play
 import dev.bitspittle.racketeer.console.game.GameContext
 import dev.bitspittle.racketeer.console.command.Command
 import dev.bitspittle.racketeer.console.view.views.game.shop.VisitShopView
+import dev.bitspittle.racketeer.model.shop.Shop
+
+fun Shop.canAffordSomething(ctx: GameContext): Boolean {
+    return (
+            ctx.state.cash >= (stock.filterNotNull().minOfOrNull { it.template.cost } ?: Int.MAX_VALUE) ||
+                    (ctx.state.shop.tier < ctx.data.maxTier && ctx.state.influence >= ctx.data.shopPrices[ctx.state.shop.tier])
+            )
+}
 
 class VisitShopCommand(ctx: GameContext) : Command(ctx) {
+    override val type = if (ctx.state.shop.canAffordSomething(ctx)) Type.Emphasized else Type.Normal
+
     override val title = "Visit shop (Tier ${ctx.state.shop.tier + 1})"
 
     override val description = "Look over the cards in the shop, or take other relevant actions."
