8d123114439fff7528a5ead93e7604fcfee7f713
David Herman <bitspittle@gmail.com>
2022-05-14 10:37:49 -0700

    Nit: Add header method to Run

--- limp/src/commonMain/kotlin/dev/bitspittle/limp/methods/system/RunMethod.kt
+++ limp/src/commonMain/kotlin/dev/bitspittle/limp/methods/system/RunMethod.kt
@@ -6,6 +6,17 @@ import dev.bitspittle.limp.Method
 import dev.bitspittle.limp.listTypeOf
 import dev.bitspittle.limp.types.Expr
 
+/**
+ * run ('Expr) ('Expr) ('Expr)... -> Any
+ *
+ * Consume all remaining arguments, which should all be expressions, and run them in order. Return the value returned
+ * from the last expression.
+ *
+ * This method is a useful way to run may expressions within an expression (e.g. a branch in an if/else condition), and
+ * to ensure that some code runs from left to right (where things are by default evaluated right to left).
+ *
+ * Any variable or method defined within a run block will be dropped by the end of it.
+ */
 class RunMethod : Method("run", 0, consumeRest = true) {
     override suspend fun invoke(
         env: Environment,
