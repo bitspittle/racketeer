16a12c2dcef0d5102185e224b1be5a1328cff5d0
David Herman <bitspittle@gmail.com>
2022-05-15 21:40:27 -0700

    Send a data snapshot upload on restart

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/system/ConfirmRestartView.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/view/views/system/ConfirmRestartView.kt
@@ -3,7 +3,14 @@ package dev.bitspittle.racketeer.console.view.views.system
 import dev.bitspittle.racketeer.console.game.GameContext
 import dev.bitspittle.racketeer.console.command.Command
 import dev.bitspittle.racketeer.console.command.commands.system.NewGameCommand
+import dev.bitspittle.racketeer.console.command.commands.system.playtestId
+import dev.bitspittle.racketeer.console.game.version
+import dev.bitspittle.racketeer.console.utils.UploadService
+import dev.bitspittle.racketeer.console.utils.encodeToYaml
 import dev.bitspittle.racketeer.console.view.views.game.GameView
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class ConfirmRestartView(ctx: GameContext) : GameView(ctx) {
     override fun createCommands(): List<Command> = listOf(
@@ -14,6 +21,23 @@ class ConfirmRestartView(ctx: GameContext) : GameView(ctx) {
             override val description = "Press ENTER if you're sure you want to restart this game. Otherwise, go back!"
 
             override suspend fun invoke(): Boolean {
+                // The user restarted. Why? Let's send some data, maybe we can find out.
+                if (!ctx.settings.admin.enabled) {
+                    ctx.app.uploadService.upload(
+                        buildString {
+                            append("versions:${ctx.app.version}:")
+                            append("users:${ctx.app.userData.playtestId}:")
+                            val utcNow =
+                                Instant.now().atOffset(ZoneOffset.UTC)
+                                    .format(DateTimeFormatter.ofPattern("MM-dd-yyyy|HH:mm:ss"))
+                            append("restarts:$utcNow:")
+                            append("turn:${ctx.state.turn}:vp:${ctx.state.vp}")
+                            append(".yaml")
+                        },
+                        UploadService.MimeTypes.YAML
+                    ) { ctx.encodeToYaml() }
+                }
+
                 return NewGameCommand(ctx).invoke()
             }
         }
