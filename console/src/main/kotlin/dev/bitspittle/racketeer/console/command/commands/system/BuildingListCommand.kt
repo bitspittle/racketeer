afe6ab60e793d04ee6644d7d971e45044a86b9d1
David Herman <bitspittle@gmail.com>
2022-05-19 15:05:11 -0700

    Hide the building list until buildings are unlocked

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/system/BuildingListCommand.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/command/commands/system/BuildingListCommand.kt
@@ -5,6 +5,7 @@ import dev.bitspittle.racketeer.console.game.GameContext
 import dev.bitspittle.racketeer.console.view.views.game.buildings.BuildingListView
 
 class BuildingListCommand(ctx: GameContext) : Command(ctx) {
+    override val type = if (ctx.settings.unlocks.buildings) Type.Normal else Type.Hidden
     override val title = "Building list"
     override val description: String = "Look through all the buildings in the game and see what you've built (${ctx.data.blueprints.count { ctx.userStats.buildings.contains(it.name) } * 100 / ctx.data.blueprints.size}%)."
     override suspend fun invoke(): Boolean {
