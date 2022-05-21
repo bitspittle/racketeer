aab264fa04cfe94efa80a372a375385676ee53d4
David Herman <bitspittle@gmail.com>
2022-05-21 14:47:13 -0700

    Made ViewStack thread safe
    ... after seeing a concurrent modification exception earlier

--- console/src/main/kotlin/dev/bitspittle/racketeer/console/view/ViewStack.kt
+++ console/src/main/kotlin/dev/bitspittle/racketeer/console/view/ViewStack.kt
@@ -1,6 +1,21 @@
 package dev.bitspittle.racketeer.console.view
 
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.write
+
+/**
+ * The current stack of screens.
+ *
+ * The user will always see the top most screen, but the stack exists so they can request going
+ * back.
+ *
+ * Note: This class is accessed from multiple threads, so it's implementation must be designed to be thread safe.
+ * Callers can use [lock] if they need to ensure that the view stack won't change while they're doing something.
+ */
 interface ViewStack {
+    val lock: ReentrantReadWriteLock
+
     val canGoBack: Boolean
     val currentView: View
 
@@ -14,12 +29,17 @@ interface ViewStack {
 /** Keep popping screens until the passed in predicate is true (or we hit bottom) */
 fun ViewStack.popUntil(pred: (View) -> Boolean) {
     @Suppress("ControlFlowWithEmptyBody") // popView's side effect is all we need
-    while (this.popView() && !pred(this.currentView)) {}
+
+    lock.write {
+        while (this.popView() && !pred(this.currentView)) { }
+    }
 }
 
 fun ViewStack.popUntilAndRefresh(pred: (View) -> Boolean) {
-    popUntil(pred)
-    currentView.refreshCommands()
+    lock.write {
+        popUntil(pred)
+        currentView.refreshCommands()
+    }
 }
 
 /** Keep popping screens until we hit bottom. */
@@ -28,8 +48,10 @@ fun ViewStack.popAll() {
 }
 
 fun ViewStack.popAllAndRefresh() {
-    popAll()
-    currentView.refreshCommands()
+    lock.write {
+        popAll()
+        currentView.refreshCommands()
+    }
 }
 
 /** Keep popping screens until one AFTER we match the predicate, useful for going on past some root screen. */
@@ -46,37 +68,47 @@ fun ViewStack.popPast(pred: (View) -> Boolean) {
 }
 
 fun ViewStack.popPastAndRefresh(pred: (View) -> Boolean) {
-    popPast(pred)
-    currentView.refreshCommands()
+    lock.write {
+        popPast(pred)
+        currentView.refreshCommands()
+    }
 }
 
 class ViewStackImpl : ViewStack {
-    private val _views = mutableListOf<View>()
-    val views: List<View> = _views
+    override val lock = ReentrantReadWriteLock()
 
-    override val canGoBack get() = _views.size > 1
-    override val currentView get() = views.last()
+    private val views = mutableListOf<View>()
+
+    override val canGoBack get() = lock.read { views.size > 1 }
+    override val currentView get() = lock.read { views.last() }
 
     override fun pushView(view: View) {
-        _views.add(view)
+        lock.write {
+            views.add(view)
+        }
     }
 
     override fun popView(): Boolean {
-        return if (canGoBack) {
-            _views.removeLast()
-            true
-        }
-        else {
-            false
+        return lock.write {
+            if (canGoBack) {
+                views.removeLast()
+                true
+            } else {
+                false
+            }
         }
     }
 
     override fun replaceView(view: View) {
-        _views.removeLast()
-        pushView(view)
+        lock.write {
+            views.removeLast()
+            pushView(view)
+        }
     }
 
     override fun contains(pred: (View) -> Boolean): Boolean {
-        return _views.indexOfFirst(pred) >= 0
+        return lock.read {
+            views.indexOfFirst(pred) >= 0
+        }
     }
 }
