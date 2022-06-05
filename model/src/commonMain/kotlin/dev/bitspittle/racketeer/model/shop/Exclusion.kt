d22b7f79cc2163613374c157737796030901b8b8
David Herman <bitspittle@gmail.com>
2022-06-04 23:49:33 -0700

    Remove unused "exclusion" class

--- model/src/commonMain/kotlin/dev/bitspittle/racketeer/model/shop/Exclusion.kt
+++
@@ -1,7 +0,0 @@
-package dev.bitspittle.racketeer.model.shop
-
-import dev.bitspittle.racketeer.model.card.CardTemplate
-
-class Exclusion(val expr: String, val action: suspend (CardTemplate) -> Boolean) {
-    suspend operator fun invoke(card: CardTemplate) = action.invoke(card)
-}
