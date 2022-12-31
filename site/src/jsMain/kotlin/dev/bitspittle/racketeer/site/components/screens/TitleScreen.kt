b4bbba055392f4da29c361f1923def453cf57ccf
David Herman <bitspittle@gmail.com>
2022-12-30 16:26:23 -0800

    Nit: Tweak wording

--- site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/screens/TitleScreen.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/screens/TitleScreen.kt
@@ -41,7 +41,7 @@ fun TitleScreen(
                 if (showProceedQuestion) {
                     YesNoDialog(
                         "Continue?",
-                        "Once you confirm, the existing quick save from your last game will be deleted. If you don't want this to happen, go back!"
+                        "Once you confirm, the existing quick save from your last game will be deleted. If you don't want this to happen, cancel and choose \"Restore Game\" instead!"
                     ) { yesNo ->
                         showProceedQuestion = false
                         if (yesNo == YesNo.YES) {
