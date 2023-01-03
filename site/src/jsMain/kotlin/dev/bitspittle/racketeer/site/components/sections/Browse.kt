8d770fd1fd505c5e0e1ab3b8b3de0692e8864469
David Herman <bitspittle@gmail.com>
2023-01-02 16:17:17 -0800

    Add browse pile VP to the extra side

--- site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/Browse.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/Browse.kt
@@ -2,12 +2,16 @@ package dev.bitspittle.racketeer.site.components.sections
 
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.css.Cursor
+import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.forms.Button
 import com.varabyte.kobweb.silk.components.style.*
+import com.varabyte.kobweb.silk.components.text.SpanText
+import dev.bitspittle.racketeer.model.card.vpTotal
 import dev.bitspittle.racketeer.model.pile.Pile
 import dev.bitspittle.racketeer.site.G
 import dev.bitspittle.racketeer.site.components.util.installPopup
@@ -42,8 +46,12 @@ fun BrowsePile(ctx: GameContext, pile: Pile, onDismiss: () -> Unit) {
         title = "Browsing ${ctx.describer.describePile(ctx.state, pile)}...",
         content = {
             pile.cards.sortedBy { it.template.name }.groupBy { it.template.name }.forEach { (_, cards) ->
-                Div(ReadOnlyStyle.toModifier().toAttrs()) {
-                    Text(ctx.describer.describeCardGroupTitle(cards, includeTotalVp = true))
+                val totalVp = cards.sumOf { it.vpTotal }.takeIf { it > 0 }
+                Box(ReadOnlyStyle.toModifier()) {
+                    SpanText(ctx.describer.describeCardGroupTitle(cards), Modifier.margin(right = 5.px))
+                    if (totalVp != null) {
+                        SpanText("(${ctx.describer.describeVictoryPoints(totalVp)})", Modifier.align(Alignment.End).textAlign(TextAlign.End))
+                    }
                 }
                 installPopup(ctx, cards)
             }
