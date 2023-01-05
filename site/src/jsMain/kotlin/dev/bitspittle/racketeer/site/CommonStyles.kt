034b59b0487b94190101c021930a280595e57292
David Herman <bitspittle@gmail.com>
2023-01-05 15:18:37 -0800

    Nit: Fix overly aggressive css child selector

--- site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/CommonStyles.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/CommonStyles.kt
@@ -5,7 +5,7 @@ import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.style.*
 
 val FullWidthChildrenStyle = ComponentStyle("full-width-children") {
-    cssRule(" *") {
+    cssRule(" > *") {
         Modifier.fillMaxWidth()
     }
 }
