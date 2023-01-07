d63750d59e8ae77487bb092769f1a7d67cc381f9
David Herman <bitspittle@gmail.com>
2023-01-06 17:03:59 -0800

    Add "x out of y" messaging to building and card list menus

--- site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/user/BuildingList.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/user/BuildingList.kt
@@ -2,24 +2,40 @@ package dev.bitspittle.racketeer.site.components.sections.menu.menus.user
 
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.css.WhiteSpace
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.foundation.layout.RowScope
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.thenUnless
 import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.style.*
 import com.varabyte.kobweb.silk.components.style.common.DisabledStyle
 import com.varabyte.kobweb.silk.components.text.SpanText
+import dev.bitspittle.racketeer.site.FullWidthChildrenStyle
 import dev.bitspittle.racketeer.site.components.sections.ReadOnlyStyle
 import dev.bitspittle.racketeer.site.components.sections.menu.Menu
 import dev.bitspittle.racketeer.site.components.sections.menu.MenuActions
 import dev.bitspittle.racketeer.site.components.util.PopupParams
 import dev.bitspittle.racketeer.site.components.util.installPopup
+import dev.bitspittle.racketeer.site.components.widgets.ArrowButton
+import dev.bitspittle.racketeer.site.components.widgets.ArrowDirection
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 
 class BuildingListMenu(private val params: PopupParams) : Menu {
     override val title = "Building List"
+
+    override val topRow: @Composable RowScope.() -> Unit = {
+        SpanText(
+            "You have built ${params.userStats.buildings.size} out of ${params.data.blueprints.size} buildings.",
+            Modifier.margin(topBottom = 10.px).fillMaxWidth().textAlign(TextAlign.Center)
+        )
+    }
+
 
     @Composable
     override fun renderContent(actions: MenuActions) = with(params) {
--- site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/user/CardList.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/user/CardList.kt
@@ -3,8 +3,10 @@ package dev.bitspittle.racketeer.site.components.sections.menu.menus.user
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.css.WhiteSpace
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.foundation.layout.RowScope
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.thenUnless
@@ -14,6 +16,7 @@ import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.style.*
 import com.varabyte.kobweb.silk.components.style.common.DisabledStyle
 import com.varabyte.kobweb.silk.components.text.SpanText
+import dev.bitspittle.racketeer.site.FullWidthChildrenStyle
 import dev.bitspittle.racketeer.site.components.sections.ReadOnlyStyle
 import dev.bitspittle.racketeer.site.components.sections.menu.Menu
 import dev.bitspittle.racketeer.site.components.sections.menu.MenuActions
@@ -36,24 +39,27 @@ class CardListMenu(private val params: PopupParams) : Menu {
     private var sortingOrder by mutableStateOf(SortingOrder.NAME)
 
     override val topRow: @Composable RowScope.() -> Unit = {
-        SimpleGrid(
-            numColumns(4), Modifier
-                .fillMaxWidth()
-                .gap(10.px)
-                .whiteSpace(WhiteSpace.NoWrap)
-                .gridTemplateColumns("min-content min-content 1fr min-content")
-        ) {
-            SpanText("Sorted by:")
-            ArrowButton(ArrowDirection.LEFT) {
-                sortingOrder =
-                    SortingOrder.values()[(sortingOrder.ordinal - 1 + SortingOrder.values().size) % SortingOrder.values().size]
-            }
-            SpanText(
-                sortingOrder.name.lowercase().capitalize(),
-                Modifier.textAlign(TextAlign.Center)
-            )
-            ArrowButton(ArrowDirection.RIGHT) {
-                sortingOrder = SortingOrder.values()[(sortingOrder.ordinal + 1) % SortingOrder.values().size]
+        Column(Modifier.fillMaxWidth().gap(20.px).margin(topBottom = 10.px), horizontalAlignment = Alignment.CenterHorizontally) {
+            SpanText("You have owned ${params.userStats.cards.size} out of ${params.data.cards.size} cards.")
+
+            SimpleGrid(
+                numColumns(4), Modifier
+                    .gap(10.px)
+                    .whiteSpace(WhiteSpace.NoWrap)
+                    .gridTemplateColumns("min-content min-content 60px min-content")
+            ) {
+                SpanText("Sorted by:")
+                ArrowButton(ArrowDirection.LEFT) {
+                    sortingOrder =
+                        SortingOrder.values()[(sortingOrder.ordinal - 1 + SortingOrder.values().size) % SortingOrder.values().size]
+                }
+                SpanText(
+                    sortingOrder.name.lowercase().capitalize(),
+                    Modifier.textAlign(TextAlign.Center)
+                )
+                ArrowButton(ArrowDirection.RIGHT) {
+                    sortingOrder = SortingOrder.values()[(sortingOrder.ordinal + 1) % SortingOrder.values().size]
+                }
             }
         }
     }
