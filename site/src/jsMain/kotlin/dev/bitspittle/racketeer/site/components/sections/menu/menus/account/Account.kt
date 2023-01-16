db773ada25eaa857a9bc6fe7590bb01857f7aff1
David Herman <bitspittle@gmail.com>
2023-01-15 22:31:31 -0800

    Center "signed in" text

--- site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/account/Account.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/account/Account.kt
@@ -3,22 +3,16 @@ package dev.bitspittle.racketeer.site.components.sections.menu.menus.account
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.foundation.layout.RowScope
-import com.varabyte.kobweb.compose.foundation.layout.Spacer
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.forms.Button
 import com.varabyte.kobweb.silk.components.text.SpanText
 import dev.bitspittle.racketeer.site.components.sections.menu.Menu
 import dev.bitspittle.racketeer.site.components.sections.menu.MenuActions
-import dev.bitspittle.racketeer.site.components.sections.menu.MenuButton
 import dev.bitspittle.racketeer.site.components.util.Data
 import dev.bitspittle.racketeer.site.components.util.PopupParams
 import dev.bitspittle.racketeer.site.components.widgets.YesNo
 import dev.bitspittle.racketeer.site.components.widgets.YesNoDialog
-import dev.bitspittle.racketeer.site.model.Settings
-import dev.bitspittle.racketeer.site.model.isDefault
-import dev.bitspittle.racketeer.site.model.user.clear
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.web.dom.*
 
@@ -27,7 +21,7 @@ class AccountMenu(private val params: PopupParams) : Menu {
 
     override val topRow: @Composable RowScope.() -> Unit = {
         params.account.email?.let { email ->
-            SpanText("You are signed is as ${email}.", Modifier.textAlign(TextAlign.Center))
+            SpanText("You are signed is as ${email}.", Modifier.fillMaxWidth().textAlign(TextAlign.Center))
         }
     }
 
