3bff7b687842c4cb07ca58a0f94febfbeaf25303
David Herman <bitspittle@gmail.com>
2023-01-18 12:03:02 -0800

    Nit: Tweak "quit" message so it reads better with Yes/No

--- site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/game/Main.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/racketeer/site/components/sections/menu/menus/game/Main.kt
@@ -72,7 +72,7 @@ class MainMenu(private val params: GameMenuParams) : Menu {
             if (showConfirmQuestion) {
                 YesNoDialog(
                     "Are you sure?",
-                    "You will lose your game if you quit.\n\nConsider running Quit & Save instead."
+                    "You will lose your game if you quit.\n\nConsider running Quit & Save instead.\n\nContinue?"
                 ) { yesNo ->
                     showConfirmQuestion = false
                     if (yesNo == YesNo.YES) {
