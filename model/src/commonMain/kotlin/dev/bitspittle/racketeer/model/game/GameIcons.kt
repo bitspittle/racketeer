74f0e8c30267e51e993e90f3faa780ecbe1bffb6
David Herman <bitspittle@gmail.com>
2023-03-07 11:41:11 -0800

    Add icons for various game areas

--- model/src/commonMain/kotlin/dev/bitspittle/racketeer/model/game/GameIcons.kt
+++ model/src/commonMain/kotlin/dev/bitspittle/racketeer/model/game/GameIcons.kt
@@ -4,12 +4,20 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GameIcons(
+    val buildings: String,
     val cash: String,
+    val deck: String,
+    val discard: String,
     val expendable: String,
     val flavor: String,
+    val hand: String,
     val influence: String,
+    val jail: String,
     val luck: String,
+    val move: String,
     val new: String,
+    val shop: String,
+    val street: String,
     val suspicious: String,
     val swift: String,
     val tweak: String,
